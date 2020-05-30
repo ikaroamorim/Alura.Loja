c68285f62b5839e297e1bb74e0b16522aa7dfe0d
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-05-30 20:23:03 -0300

    Primeira adição de produto no db.

---
+++ Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+  internal class LojaContext : DbContext
+  {
+    public DbSet<Produto> Produtos { get; set; } //Nome da tabela do banco de dados
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    {
+      optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
+    }
+  }
+}
+
+/*Data Source = (localdb)\MSSQLLocalDB;Initial Catalog = LojaDB; Integrated Security = True; Connect Timeout = 30; Encrypt=False;TrustServerCertificate=False;ApplicationIntent=ReadWrite;MultiSubnetFailover=False*/
--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -6,24 +6,52 @@ using System.Threading.Tasks;
 
 namespace Alura.Loja.Testes.ConsoleApp
 {
-    class Program
+  class Program
+  {
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            GravarUsandoAdoNet();
-        }
-
-        private static void GravarUsandoAdoNet()
-        {
-            Produto p = new Produto();
-            p.Nome = "Harry Potter e a Ordem da Fênix";
-            p.Categoria = "Livros";
-            p.Preco = 19.89;
-
-            using (var repo = new ProdutoDAO())
-            {
-                repo.Adicionar(p);
-            }
-        }
+      //GravarUsandoAdoNet();
+      GravarUsandoEntity();
     }
+
+    private static void GravarUsandoEntity()
+    {
+      Produto p1 = new Produto();
+      p1.Nome = "Harry Potter e a Ordem da Fênix";
+      p1.Categoria = "Livros";
+      p1.Preco = 19.89;
+
+      Produto p2 = new Produto();
+      p2.Nome = "Senhor dos Anéis 1";
+      p2.Categoria = "Livros";
+      p2.Preco = 19.89;
+
+      Produto p3 = new Produto();
+      p3.Nome = "O Monge e o Executivo";
+      p3.Categoria = "Livros";
+      p3.Preco = 19.89;
+
+      using (var contexto = new LojaContext())
+      {
+        //contexto.Produtos.Add(p1);
+        //contexto.Produtos.Add(p2);
+        //contexto.Produtos.Add(p3);
+        contexto.AddRange(p1, p2, p3);
+        contexto.SaveChanges();
+      }
+    }
+
+    private static void GravarUsandoAdoNet()
+    {
+      Produto p = new Produto();
+      p.Nome = "Harry Potter e a Ordem da Fênix";
+      p.Categoria = "Livros";
+      p.Preco = 19.89;
+
+      using (var repo = new ProdutoDAO())
+      {
+        repo.Adicionar(p);
+      }
+    }
+  }
 }
