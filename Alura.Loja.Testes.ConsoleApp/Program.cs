67df236ad886cd5b2c6f5d6eec863c9904e083c7
Ikaro Amorim e Silva <ikaro.amorim@gmail.com>
2020-05-30 22:02:54 -0300

    Conclusão do CRUD e implementação da Produto DAO Entity para comparar com a Produto DAO

--- Alura.Loja.Testes.ConsoleApp/Program.cs
+++ Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -12,14 +12,56 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
       //GravarUsandoAdoNet();
       GravarUsandoEntity();
+      RecuperarProdutos();
+      //ExcluirProdutos();
+      //RecuperarProdutos();
+      AtualizarProduto();
+      RecuperarProdutos();
+    }
+
+    private static void AtualizarProduto()
+    {
+
+      using (var repo = new LojaContext())
+      {
+        Produto primeiro = repo.Produtos.First();
+        primeiro.Nome = "Cassino Royale - Editado";
+        repo.SaveChanges();
+      }
+    }
+
+    private static void ExcluirProdutos()
+    {
+      using(var repo = new LojaContext())
+      {
+        IList<Produto> produtos = repo.Produtos.ToList();
+        foreach (var item in produtos)
+        {
+          repo.Produtos.Remove(item);
+        }
+        repo.SaveChanges();
+      }
+    }
+
+    private static void RecuperarProdutos()
+    {
+      using (var repo = new LojaContext())
+      {
+        IList<Produto> produtos = repo.Produtos.ToList();
+        Console.WriteLine($"Foram encontrados: {produtos.Count} produtos");
+        foreach (var item in produtos)
+        {
+          Console.WriteLine(item.Nome);
+        }
+      }
     }
 
     private static void GravarUsandoEntity()
     {
       Produto p1 = new Produto();
-      p1.Nome = "Harry Potter e a Ordem da Fênix";
+      p1.Nome = "O Carteiro e o Poeta";
       p1.Categoria = "Livros";
-      p1.Preco = 19.89;
+      p1.Preco = 13.99;
 
       Produto p2 = new Produto();
       p2.Nome = "Senhor dos Anéis 1";
@@ -29,7 +71,7 @@ namespace Alura.Loja.Testes.ConsoleApp
       Produto p3 = new Produto();
       p3.Nome = "O Monge e o Executivo";
       p3.Categoria = "Livros";
-      p3.Preco = 19.89;
+      p3.Preco = 25.49;
 
       using (var contexto = new LojaContext())
       {
